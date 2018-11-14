2d26f662b550733c3643bca02ddd6f3d9cf39b79
Daniel Lopez <Daniel_Lopez@intuit.com>
2018-11-13 16:11:59 -0800

    structural init commit

---
+++ src/test/java/com/memeeconomy/memeeconomyapi/MemeEconomyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.memeeconomy.memeeconomyapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemeEconomyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
